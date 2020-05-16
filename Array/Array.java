943e9c60906a9e0f0b2b8104c2c01b3fa11ef6c3
vepailjanov <https://github.com/vepashka2019/firsyGitfile.git>
2020-05-16 09:29:32 -0400

    firstgit

---
+++ Array/Array.java
@@ -0,0 +1,14 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,0};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
+
+}
